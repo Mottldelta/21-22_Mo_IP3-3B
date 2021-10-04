6135b85b5c3339837fa9b85a6ce4efa573a55ba0
Jan Mottl <mottl.jan96@gmail.com>
2021-10-04 09:39:32 +0200

    Prvni hodina (zaklad, proměnné, datové typy, matematika)

--- UvodDoPHP.php
+++ UvodDoPHP.php
@@ -8,43 +8,48 @@ komentář
 na více řádků
 */
 echo "Hello world!<br>";
-echo /*komentář uvnitř kodu*/ "hello<br>";
+echo /*komentář uvnitř kodu*/
+"hello<br>";
 
 //proměnné
 $c = "Hello world";
-echo "Chtěl bych vám říct: ". $c ."!<br>";
+echo "Chtěl bych vám říct: " . $c . "!<br>";
 $x = 7;
 $y = 6;
-echo $y + $x ."<br>";
+echo $y + $x . "<br>";
 
 //prvni funkce
-function nasobeni() {
+function nasobeni()
+{
     $a = 4;
     $b = 5;
-    echo $a * $b ."<br>";
+    echo $a * $b . "<br>";
 }
 
 nasobeni();
 
 //funkce global
-function nasobeni2() {
+function nasobeni2()
+{
     global $x, $y;
-    echo $x * $y ."<br>";
+    echo $x * $y . "<br>";
 }
 
 nasobeni2();
 
 //funkce $GLOBALS
-function nasobeni3() {
+function nasobeni3()
+{
     $z = $GLOBALS['x'] * $GLOBALS['y'];
-    echo $z ."<br>";
+    echo $z . "<br>";
 }
 
 nasobeni3();
 
 //funkce statická proměnná
 
-function staticka() {
+function staticka()
+{
     static $p = 0;
     echo $p;
     $p = $p + 2;
@@ -78,10 +83,10 @@ var_dump($n);
 //datový typ textový řetězec
 $txt = "Hello World! ";
 $name = "Steve";
-echo strlen($txt) ."<br>";
-echo str_word_count($txt) ."<br>";
-echo strrev($txt) ."<br>";
-echo strpos($txt, "World!") ."<br>";
+echo strlen($txt) . "<br>";
+echo str_word_count($txt) . "<br>";
+echo strrev($txt) . "<br>";
+echo strpos($txt, "World!") . "<br>";
 echo str_replace("Hello", "GoodMorning", $txt) . "<br>";
 echo $txt . $name . "<br>";
 
@@ -95,14 +100,18 @@ echo str_replace("Steve", "Jan", $text);
 $o = 1.;
 var_dump(is_float($o));
 $int_cast = (int)$o;
-echo $int_cast ."<br>";
+echo $int_cast . "<br>";
 
 
 //Matematika
-echo pi() ."<br>";
+echo pi() . "<br>";
 $cisla = array(5, 6, 8, 10, 12);
-echo min($cisla) ."<br>";
-echo max($cisla) ."<br>";
+echo min($cisla) . "<br>";
+echo max($cisla) . "<br>";
+
+//2. hodina – pokračování v úvodu do PHP
+
+echo "2. hodina – pokračování v úvodu do PHP"
 ?>
 
 
