05452dc49fc1b1aa10f78b95aa84f38ddbbc44bd
Jan Mottl <mottl.jan96@gmail.com>
2022-01-10 10:57:48 +0100

    MySQL (admin banky) - komplet

--- MySQL/banka/detail.php
+++ MySQL/banka/detail.php
@@ -15,20 +15,27 @@ $result = $conn->query($sql);
 if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
+        $ucet = $row["currency"];
         echo "<b>" .$row["name"] . "</b>" . "<br>" . $row["email"] . "<br>" . $row["country"] . "<br> Zůstatek na účtu: " . $row["currency"] . "<br>";
+
     }
 } else {
     echo "0 results";
 }
 $conn->close();
 
+$uspory = (($ucet/100)*5);
 
-$adress1 = "edit.php?idzak=$id&suma=20";
+$adress1 = "edit.php?idzak=$id&ukon=1&suma=20";
 echo "<br>" . "<a href= ". $adress1 . ">Strhnout poplatek za kreditní kartu</a><br>";
-$adress2 = "edit.php?idzak=$id&suma=100";
+$adress2 = "edit.php?idzak=$id&ukon=1&suma=100";
 echo "<a href= ". $adress2 . ">Strhnout poplatek za účet</a><br>";
-$adress3 = "edit.php?idzak=$id&suma=";
+$adress3 = "edit.php?idzak=$id&ukon=1&suma=$ucet";
 echo "<a href= ". $adress3 . ">Vynulovat účet</a><br>";
+$adress4 = "edit.php?idzak=$id&ukon=2&suma=$uspory";
+echo "<a href= ". $adress4 . ">Započíst úspory</a><br>";
+$adress5 = "edit.php?idzak=$id&ukon=3&suma=0";
+echo "<a href= ". $adress5 . ">Smazat účet</a><br>";
 $adress = "seznam.php";
-echo  "<a href= ". $adress . ">Zpět na seznam zákazníků!</a><br>";
+echo  "<br>" . "<a href= ". $adress . ">Zpět na seznam zákazníků!</a><br>";
 ?>
--- MySQL/banka/edit.php
+++ MySQL/banka/edit.php
@@ -6,19 +6,36 @@ $dbname = "banks";
 
 $id = $_GET['idzak'];
 $sum = $_GET['suma'];
+$task = $_GET['ukon'];
 try {
     $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    if ($task == 1) {
     $sql = "UPDATE customers SET currency = currency - $sum WHERE id = $id";
+    } elseif ($task == 2) {
+        $sql = "UPDATE customers SET currency = currency + $sum WHERE id = $id";
+    } elseif ($task == 3) {
+        $sql = "DELETE FROM customers WHERE id = $id"; }
     // use exec() because no results are returned
     $conn->exec($sql);
-    echo "Poplatek byl stržen!";
+        if ($task == 1) {
+            echo "Poplatek byl stržen!";
+        } elseif ($task == 2) {
+            echo "Úspory byly započteny!";
+        } elseif ($task == 3) {
+            echo "Účet byl smazán!";; }
 } catch(PDOException $e) {
     echo $sql . "<br>" . $e->getMessage();
 }
 $conn = null;
 
+        if($task != 3) {
 $adress = "detail.php?id=$id";
 echo "<br>" . "<a href= ". $adress . ">Zpět na detail uživatele!</a>";
+        } else {
+            $adress = "index.php";
+            echo "<br>" . "<a href= ". $adress . ">Zpět na hlavní stránku!</a>";
+
+}
 ?>
