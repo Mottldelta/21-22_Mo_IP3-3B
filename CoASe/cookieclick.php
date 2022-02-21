6dbd19c32e335c1e59a719d2ad26c7424f3a2b46
Jan Mottl <mottl.jan96@gmail.com>
2022-02-21 11:10:13 +0100

    cookie (click v.2)

--- CoASe/cookieclick.php
+++ CoASe/cookieclick.php
@@ -1,6 +1,7 @@
 <?php
 if(!isset($_COOKIE['klik'])) {
     setcookie('klik',0,time()+1000,"/");
+    echo "Počet kliknutí je " . $_COOKIE['klik'] . "<br>";
 } else {
     echo "Počet kliknutí je " . $_COOKIE['klik'] . "<br>";
 }
@@ -9,12 +10,17 @@ if(!isset($_COOKIE['klik'])) {
 
 <html>
 <form>
-    <button onclick="<?php click() ?>">CLICK<button>
+    <button onclick="<?php click() ?>">CLICK</button>
+    <button onclick="<?php resett() ?>">RESET</button>
 </form>
 </html>
 
 <?php
 function click() {
-
+    $click = $_COOKIE['klik'] + 1;
+    setcookie('klik',$click,time()+1000,"/");
+}
+function resett() {
+    setcookie('klik',0,time()-1000,"/");
 }
 ?>
