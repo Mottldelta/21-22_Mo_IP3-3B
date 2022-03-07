eda351bc91a11dd36947209296e5926fdaf3c4d7
Jan Mottl <mottl.jan96@gmail.com>
2022-03-07 10:57:26 +0100

    session v.2

--- CoASe/sescheck.php
+++ CoASe/sescheck.php
@@ -1,5 +1,15 @@
 <?php
+session_start();
 $user = "username";
 $pass = 12345;
 
+$zadjmeno = $_POST['jmeno'];
+$zadheslo = $_POST['heslo'];
+
+if ($zadjmeno == $user and $zadheslo == $pass) {
+    echo "Přihlásil jste se!";
+    $_SESSION['login'] = 1;
+} else {
+    echo "špatné přihlašovací údaje";
+}
 ?>
--- CoASe/seslogin.php
+++ CoASe/seslogin.php
@@ -1,2 +1,29 @@
 <?php
+session_start();
+if(isset($_SESSION['login'])) {
+    echo "Jste přihlášen";
+}
 ?>
+<html lang="cs">
+<head>
+    <meta charset="UTF-8">
+    <title>HMTL Form</title>
+</head>
+<body>
+<h1>Login</h1>
+<form action="sescheck.php" method="post">
+    <!--    Label s textovým polem-->
+    <label for="custname">Jméno: </label>
+    <input type="text" name="jmeno" id="custname" placeholder="Napiš své jméno" autofocus> <br>
+    <!--        Heslo-->
+    <label for="custpass">Heslo: </label>
+    <input type="password" id="custpass" name="heslo">
+    <br>
+
+    <!--    Tlačítko pro odeslání formuláře-->
+    <input type="submit">
+    <br>
+</form>
+
+</body>
+</html>
