5fbb878042158dd921401a08fd71ca1b02eb1226
Jan Mottl <mottl.jan96@gmail.com>
2022-03-21 11:12:50 +0100

    práce se soubory v.2

--- PraceSoubory/pracesoubor.php
+++ PraceSoubory/pracesoubor.php
@@ -1,13 +1,34 @@
 <?php
 //readfile('text.txt');
 
-//otveřit
+//otveřit pro čtení
 $soubor = fopen("text.txt","r");
 //čtení z otevřeného souboru
 echo fread($soubor,1);
 //číst jeden řádek z otevřeného souboru.
 echo fgets($soubor);
 echo fgets($soubor);
-//zavířt
+//čtení soubor po řádcích až do konce samotného souboru
+/*while(!feof($soubor)){
+    echo fgets($soubor);
+}*/
+//čtení souboru po znaku
+while(!feof($soubor)) {
+    echo fgetc($soubor);
+}
+//zavířt soubor
 fclose($soubor);
+
+//soubor otevřít pro psaní
+$soubor2 = fopen("text.txt","w");
+//psaní do souboru
+fwrite($soubor2,"Ahoj Lukáši\nAhoj Davide\n");
+//zavířt soubor
+fclose($soubor2);
+
+//Pro přepis nutný jiný mód
+$soubor3 = fopen("text.txt","a");
+fwrite($soubor3,"Ahoj Lukáši\nAhoj Davide");
+fclose($soubor3);
+
 ?>
